ce8d341ebcc28ce04d2bd02b93c7e93f69637a9d
Anarebo <a.reboredo93@gmail.com>
2022-03-20 14:06:49 -0300

    proyect

--- PIntegAr/karma.conf.js
+++ PIntegAr/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/proy-integ-ar'),
+      dir: require('path').join(__dirname, './coverage/pinteg-ar'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- PIntegAr/src/app/app.component.spec.ts
+++ PIntegAr/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProyIntegAR'`, () => {
+  it(`should have as title 'PIntegAr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProyIntegAR');
+    expect(app.title).toEqual('PIntegAr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProyIntegAR app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PIntegAr app is running!');
   });
 });
